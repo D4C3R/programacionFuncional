c14003489a937ddcc7a4bcb61605f43739c8298e
Darielforward <dcerdas@fwdcostarica.com>
2024-08-14 15:14:06 -0600

    ejercicio

--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,1 +1,11 @@
-console.log('hola2');
+let contador = {
+    valor: 0,
+    siguiente: function () {
+      this.valor++
+      return this.valor
+    },
+  };
+  
+  console.log(contador.siguiente());
+
+  
