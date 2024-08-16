2ae3464b44e641a1e4fa820ae34f32bebce1ca66
Darielforward <dcerdas@fwdcostarica.com>
2024-08-16 14:24:12 -0600

    ejercicio4

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { sumar, restar, multiplicar, dividir} from "./calculadora.js"
+
+
+console.log(sumar(2,2));
+console.log(restar(74,5));
+console.log(multiplicar(210,2));
+console.log(dividir(6216,8));
